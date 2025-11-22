87511179b538a107fcfdffa5b24d1b271053be75
someshwar-kumbar <tncxsomu@gmail.com>
2025-11-22 11:03:11 +0530

    Update student.py

--- student.py
+++ student.py
@@ -7,8 +7,8 @@ if len(sys.argv) == 3:
     print("user provided input values:")
 else:
   script_name = sys.argv[0] 
-  name = sys.argv[1]
-  rollno = sys.argv[2]  
+  name = "somu"
+  rollno = "bca307"
   print("no input given -using default values")
 
 print("script name :", script_name)

88ac09fab06098c2884f37f572025cdb290f6ac1
someshwar-kumbar <tncxsomu@gmail.com>
2025-11-22 10:55:27 +0530

    Create student.py

---
+++ student.py
@@ -0,0 +1,16 @@
+import sys
+
+if len(sys.argv) == 3:
+    script_name=sys.argv[0]
+    name = sys.argv[1]
+    rollno = sys.argv[2]
+    print("user provided input values:")
+else:
+  script_name = sys.argv[0] 
+  name = sys.argv[1]
+  rollno = sys.argv[2]  
+  print("no input given -using default values")
+
+print("script name :", script_name)
+print("student name :", name)
+print("roll number :", rollno)
